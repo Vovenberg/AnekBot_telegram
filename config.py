e71899f0be16a9e2352c29390e2aab2ee80b2781
vladimir <altair194@yandex.ru>
2017-07-05 01:10:13 +0400

    add 3 button with different functional

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 token = '422504187:AAGKOYJKFs4y6BTbKKzR172Jn7Lyow8oTFk'
 url = "https://api.vk.com/method/wall.get?domain=baneks&count=10&access_token=bca0978ebca0978ebca0978e86bcfd9b32bbca0bca0978ee5f992d121dcfc8ee1f5bbcc"
+urlTop = "https://api.vk.com/method/wall.get?domain=baneks&count=100&access_token=bca0978ebca0978ebca0978e86bcfd9b32bbca0bca0978ee5f992d121dcfc8ee1f5bbcc"
+urlRandom = "https://api.vk.com/method/wall.get?domain=baneks&posts={}&access_token=bca0978ebca0978ebca0978e86bcfd9b32bbca0bca0978ee5f992d121dcfc8ee1f5bbcc"
--- main.py
+++ main.py
@@ -3,26 +3,76 @@ import config
 import eventlet
 import requests
 import logging
+import random
+from telebot import types
 
 bot = telebot.TeleBot(config.token)
 
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    posts = get_data()
-    send_messages(posts, message.chat.id)
+    buttons = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    buttons.add("Последние 10")
+    buttons.add( "ТОП10")
+    buttons.add("Случайный анекдот")
+    bot.send_message(chat_id=message.chat.id, text="Анекдот в студию!", reply_markup=buttons)
+    bot.register_next_step_handler(message, process_step)
 
 
+def process_step(message):
+    chat_id = message.chat.id
+    if message.text == 'Последние 10':
+        get_last_10(chat_id)
+    if message.text == 'ТОП10':
+        get_top(chat_id)
+    if message.text == 'Случайный анекдот':
+        get_random(chat_id)
+
+
+def get_top(chat_id):
+    posts = get_data(config.urlTop)
+    posts.sort(key=sortByLikes)
+    send_messages(posts[89:99], chat_id)
+
+
+def get_last_10(chat_id):
+    posts = get_data(config.url)
+    send_messages(posts, chat_id)
+
+
+def get_random(chat_id):
+    # id = random.randint(554000, 555000).__str__()
+    # url = config.urlRandom.format('-45491419_'.__add__(id))
+    posts = get_data(config.urlTop)[random.randint(0,99)]
+    send_messages(posts, chat_id)
+
+
+#########################################
 def send_messages(posts, chat_id):
-    for i, post in enumerate(posts['response']):
-        if (i >= 1):
-            text_ = post["text"].replace("<br>", "\n").__add__('\n\n------№{}---------'.format(i+1))
-            bot.send_message(chat_id, text_)
+    if (isinstance(posts, list)):
+        text_array = []
+        for i, post in enumerate(posts):
+            text = '\n\n------№{}---------\n'.format(i + 1).__add__(post["text"].replace("<br>", "\n")).__add__(
+                '\n\nРейтинг лайков: {}'.format(post['likes']["count"]))
+            text_array.append(text)
+            if (i%3 == 0):
+                join = ''.join(text_array)
+                bot.send_message(chat_id, join)
+                text_array.clear()
+    else:
+        text = posts["text"].replace("<br>", "\n").__add__(
+            '\n\nРейтинг лайков: {}'.format(posts['likes']["count"]))
+        bot.send_message(chat_id, text)
 
-def get_data():
+
+def get_data(url):
     timeout = eventlet.Timeout(10)
     try:
-        feed = requests.get(config.url)
-        return feed.json()
+        feed = requests.get(url)
+        list = []
+        for i, post in enumerate(feed.json()['response']):
+            if (i >= 1): list.append(post)
+        return list
     except eventlet.timeout.Timeout:
         logging.warning('Got Timeout while retrieving VK JSON data. Cancelling...')
         return None
@@ -31,4 +81,8 @@ def get_data():
 
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
+
+
+def sortByLikes(post):
+    return post['likes']["count"]
