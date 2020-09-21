852672d057a3903d11464eb78f660b2353c50ed1
Vladimir Kildyushev <vladimir.kildushev@simbirsoft.com>
2020-09-21 22:12:52 +0400

    fix api vk with version

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 CHAT_ID = '258260689'
 FILENAME_LASTID = "lastpost"
 token = '422504187:AAFWFSoV2GXL_sCAnWL4xcGkk47SqmPIJPY'
-urlCategoryB = "https://api.vk.com/method/wall.get?domain=baneks&count={}&offset={}&access_token=bca0978ebca0978ebca0978e86bcfd9b32bbca0bca0978ee5f992d121dcfc8ee1f5bbcc"
-urlUmoreski = "https://api.vk.com/method/wall.get?domain=jumoreski&count={}&offset={}&access_token=bca0978ebca0978ebca0978e86bcfd9b32bbca0bca0978ee5f992d121dcfc8ee1f5bbcc"
+urlCategoryB = "https://api.vk.com/method/wall.get?domain=baneks&count={}&offset={}&v=5.124&access_token=bca0978ebca0978ebca0978e86bcfd9b32bbca0bca0978ee5f992d121dcfc8ee1f5bbcc"
+urlUmoreski = "https://api.vk.com/method/wall.get?domain=jumoreski&count={}&offset={}&v=5.124&access_token=bca0978ebca0978ebca0978e86bcfd9b32bbca0bca0978ee5f992d121dcfc8ee1f5bbcc"
 urlApp = "http://anekbot.herokuapp.com"
